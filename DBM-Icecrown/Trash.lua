0bfda49cd9e76b806d76ce498c643d5d0e245da7
Zidras <10605951+Zidras@users.noreply.github.com>
2024-08-04 18:53:07 +0100

    ICC-Trash: adjust Zombies timer for Precious
    Precious does not have anything to track when it is engaged. It is not foreseen to implement engage detection.

--- DBM-Icecrown/Trash.lua
+++ DBM-Icecrown/Trash.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 local UnitGUID = UnitGUID
 
-mod:SetRevision("20240804184235")
+mod:SetRevision("20240804185315")
 mod:SetModelID(37007)
 mod:SetUsedIcons(1, 2, 3, 4, 5, 6, 7, 8)
 mod.isTrashMod = true
@@ -58,7 +58,7 @@ local timerDarkReckoning		= mod:NewTargetTimer(8, 69483, nil, nil, nil, 5)
 local timerDeathPlague			= mod:NewTargetTimer(15, 72865, nil, nil, nil, 3)
 --Plagueworks
 local timerSeveredEssence		= mod:NewNextTimer(35.5, 71942, nil, nil, nil, 1, nil, nil, true) -- REVIEW! 5s variance [35.5-40.5]. Added "keep" arg, but could be a bad idea!  (25H Lordaeron [2023-08-19]@[11:49:20] || 25H Lordaeron [2023-08-27]@[10:41:16]) - 36.0 || 40.49; 35.51
-local timerZombies				= mod:NewNextTimer(20, 71159, nil, nil, nil, 1)
+local timerZombies				= mod:NewCDTimer(20, 71159, nil, nil, nil, 1) -- 5s variance [20-25]
 local timerMortalWound			= mod:NewTargetTimer(15, 71127, nil, nil, nil, 5)
 local timerDecimate				= mod:NewCDTimer(20, 71123, nil, nil, nil, 2) -- 5s variance [20-25]. Not using "keep" arg since it is quite tricky to account for timer deactivation on OOR players (would require syncs) and variance isn't that huge
 local timerBlightBomb			= mod:NewCastTimer(5, 71088, nil, nil, nil, 3)
