e6856042e1eb6c7965be0844bd79953228fc8070
Zidras <10605951+Zidras@users.noreply.github.com>
2024-08-04 10:26:34 +0100

    ICC-Festergut: revise mod for Supremacy
    To do: check Malleable Goo on Heroic

--- DBM-Icecrown/ThePlagueworks/Festergut.lua
+++ DBM-Icecrown/ThePlagueworks/Festergut.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Festergut", "DBM-Icecrown", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20240801002200")
+mod:SetRevision("20240804102443")
 mod:SetCreatureID(36626)
 mod:RegisterCombat("combat")
 mod:SetUsedIcons(1, 2, 3)
@@ -33,12 +33,12 @@ local specWarnGoo			= mod:NewSpecialWarningDodge(72297, true, nil, nil, 1, 2) --
 
 local timerGasSpore			= mod:NewBuffFadesTimer(12, 69279, nil, nil, nil, 3)
 local timerVileGas			= mod:NewBuffFadesTimer(6, 69240, nil, "Ranged", nil, 3)
-local timerGasSporeCD		= mod:NewCDTimer(40.6, 69279, nil, nil, nil, 3, nil, nil, true)	-- REVIEW! ~10s variance [40.6 - 50.3]. Added "keep" arg. (25H Lordaeron [2022-09-14]@[20:34:44] || 25H Lordaeron [2022-09-23]@[21:13:34] || 25N Lordaeron [2023-02-10]@[19:23:53] || 25N Lordaeron [2023-02-14]@[20:56:07] || 10H Icecrown [2023-04-05]@[22:46:14] || 25H Lordaeron [2023-04-07]@[19:35:55) - "Gas Spore-71221-npc:36626-1002 = pull:24.1, 42.9, 41.9, 45.9, 44.7 || "Gas Spore-71221-npc:36626-1507 = pull:24.9, 43.6, 41.5, 42.0, 41.9" || "Gas Spore-71221-npc:36626-1086 = pull:21.5, 43.9, 41.9, 43.0, 41.2, 43.5 || "Gas Spore-71221-npc:36626-1575 = pull:21.1, 44.1, 40.6, 48.7, 43.2, 44.1 || "Gas Spore-69278-npc:36626-852 = pull:20.1, 46.1, 41.2" || "Gas Spore-71221-npc:36626-1098 = pull:20.3, 43.1, 43.3, 43.6, 43.3, 43.1, 50.3"
-local timerPungentBlight	= mod:NewCDTimer(33.5, 69195, nil, nil, nil, 2)		-- Edited. ~34 seconds after 3rd stack of inhaled. REVIEW! (25H Lordaeron 2022/09/25) - pull:131.4 [33.5]
-local timerInhaledBlight	= mod:NewCDTimer(33.5, 69166, nil, nil, nil, 6, nil, nil, true)	-- Timer is based on Aura. ~9s variance on pull, 1.5s variance [33.5-35.0]. Added "keep" arg (25H Lordaeron 2022/09/04 || 25H Lordaeron 2022/09/25) - 34.2, 34.7, *, 34.2 || 34.3, 33.8, 67.5 [33.5-pungent, 34.0], 34.2
+local timerGasSporeCD		= mod:NewCDTimer(40, 69279, nil, nil, nil, 3, nil, nil, true) -- 5s variance [40-45]. Added "keep" arg.
+local timerPungentBlight	= mod:NewCDTimer(34, 69195, nil, nil, nil, 2)		-- Fixed timer after 3rd _inhaleCounter: 34s
+local timerInhaledBlight	= mod:NewCDTimer(34, 69166, nil, nil, nil, 6, nil, nil, true)	-- Timer is based on Aura. Fixed timer after first one. Added "keep" arg
 local timerGastricBloat		= mod:NewTargetTimer(100, 72219, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)	-- 100 Seconds until expired
-local timerGastricBloatCD	= mod:NewCDTimer(13.1, 72219, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON) -- REVIEW! ~3s variance [13.1 - 16.2]. (25H Lordaeron [2023-04-07]@[19:29:03] || 25H Icecrown [2023-05-28]@[16:21:49]) - "Gastric Bloat-72553-npc:36626-1098 = pull:13.8, 13.1, 13.5, 13.1, 13.2, 14.0, 13.8, 13.4, 13.2, 13.9, 13.4, 13.8 || "Gastric Bloat-72553-npc:36626-752 = pull:13.6, 16.2, 13.4, 13.1, 13.4, 13.3, 14.3, 13.4, 13.1, 14.1, 13.1, 14.8, 13.1
-local timerGooCD			= mod:NewCDTimer(10, 72297, nil, nil, nil, 3)
+local timerGastricBloatCD	= mod:NewCDTimer(15, 72219, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON) -- 2.5s variance [15-17.5]
+local timerGooCD			= mod:NewCDTimer(15, 72297, nil, nil, nil, 3) -- 5s variance [15-20]
 
 local berserkTimer			= mod:NewBerserkTimer(300)
 
@@ -72,9 +72,9 @@ end
 
 function mod:OnCombatStart(delay)
 	berserkTimer:Start(-delay)
-	timerInhaledBlight:Start(28.9-delay) -- ~5s variance [28.9-34.0] (25H Lordaeron 2022/09/04 || 25H Lordaeron 2022/09/25 || 25H Lordaeron [2022-11-16]@[22:30:46] || 10H Lordearon [2023-04-05]@[22:46:14] || 25H Lordaeron [2023-06-27]@[20:34:49] || 25H Lordaeron [2023-08-23]@[20:39:56]) - pull:32.0 || pull:32.9 || pull:28.9 || pull:30.6 || pull:34.0 || pull:30.7
-	timerGasSporeCD:Start(20-delay) -- ~5s variance [20.1 - 24.9]. Added "keep" arg. (25H Lordaeron [2022-09-14]@[20:34:44] || 25H Lordaeron [2022-09-23]@[21:13:34] || 25N Lordaeron [2023-02-10]@[19:23:53] || 25N Lordaeron [2023-02-14]@[20:56:07] || 10H Icecrown [2023-04-05]@[22:46:14] || 25H Lordaeron [2023-04-07]@[19:35:55) - pull:24.1 || pull:24.9 || pull:21.5 || pull:21.1|| pull:20.1|| pull:20.3
-	timerGastricBloatCD:Start(-delay)
+	timerInhaledBlight:Start(28.5-delay) -- 5s variance [25-30] + 3.5s cast time, since timer is using Aura Applied
+	timerGasSporeCD:Start(20-delay) -- 5s variance [20-25]
+	timerGastricBloatCD:Start(12.5-delay) -- 2.5s variance [12.5-15]
 	table.wipe(gasSporeTargets)
 	table.wipe(vileGasTargets)
 	self.vb.gasSporeCast = 0
@@ -83,7 +83,7 @@ function mod:OnCombatStart(delay)
 		DBM.RangeCheck:Show(10) -- 9.6y is the shortest distance that it doesn't spread (TC test 12/03/2023); set to 10 for safety
 	end
 	if self:IsHeroic() then
-		timerGooCD:Start(13-delay)
+		timerGooCD:Start(-delay) -- 5s variance [15-20]
 	end
 end
 
@@ -97,6 +97,7 @@ function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(69195, 71219, 73031, 73032) then	-- Pungent Blight
 		specWarnPungentBlight:Show()
 		specWarnPungentBlight:Play("aesoon")
+		timerInhaledBlight:Schedule(3)
 	end
 end
 
@@ -109,6 +110,8 @@ function mod:SPELL_CAST_SUCCESS(args)
 		else
 			timerGasSporeCD:Start()
 		end
+		-- delays EVENT_VILE_GAS by 20 seconds if current time plus the specified delay is less than event's scheduled time
+		-- there is no DBM timer for Vile Gas CD, so this is noted only for information purposes
 	end
 end
 
@@ -180,8 +183,8 @@ function mod:SPELL_AURA_REMOVED(args)
 end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName)
-	if spellName == GetSpellInfo(72299) then -- Malleable Goo Summon Trigger (10 player normal) (the other 3 spell ids are not needed here since all spells have the same name)
-		DBM:AddMsg("Malleable Goo Summon Trigger UNIT_SPELLCAST_SUCCEEDED unhidden from combat log. Notify Zidras on Discord or GitHub")
+	if spellName == GetSpellInfo(72296) then -- Malleable Goo. REVIEW!
+		DBM:AddMsg("Malleable Goo UNIT_SPELLCAST_SUCCEEDED unhidden from combat log. Notify Zidras on Discord or GitHub")
 		specWarnGoo:Show()
 		specWarnGoo:Play("watchstep")
 		if self:IsDifficulty("heroic25") then
@@ -189,7 +192,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName)
 		else
 			timerGooCD:Start(15)--30 seconds in between goos on 10 man heroic
 		end
-	elseif spellName == GetSpellInfo(73032) then -- Pungent Blight
-		timerInhaledBlight:Start()
+--	elseif spellName == GetSpellInfo(73032) then -- Pungent Blight, replaced with 3s schedule on SPELL_CAST_START
+--		timerInhaledBlight:Start()
 	end
 end
