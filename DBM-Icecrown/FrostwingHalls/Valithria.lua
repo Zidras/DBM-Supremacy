a80ef9e83962d345b6fe159d27ec5df003213a5a
Zidras <10605951+Zidras@users.noreply.github.com>
2024-08-07 18:05:02 +0100

    ICC-VDW: revise mod for Supremacy
    Still need to go over adds spawn timers

--- DBM-Icecrown/FrostwingHalls/Valithria.lua
+++ DBM-Icecrown/FrostwingHalls/Valithria.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Valithria", "DBM-Icecrown", 4)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20231121214155")
+mod:SetRevision("20240807180324")
 mod:SetCreatureID(36789)
 mod:SetUsedIcons(8)
 mod.onlyHighest = true--Instructs DBM health tracking to literally only store highest value seen during fight, even if it drops below that
@@ -34,19 +34,19 @@ local specWarnGTFO			= mod:NewSpecialWarningGTFO(71179, nil, nil, nil, 1, 8)
 local specWarnSuppressers	= mod:NewSpecialWarningSpell(70935)
 
 local timerLayWaste			= mod:NewBuffActiveTimer(12, 69325, nil, nil, nil, 2)
-local timerNextPortal		= mod:NewCDCountTimer(45, 72483, nil, nil, nil, 5, nil, DBM_COMMON_L.HEALER_ICON) -- ~3s variance. (25H Lordearon 2022/10/06 || 25H Lordearon 2022/10/09) - pull:45.0, 45.6, 47.9, 46.6 || pull:45.4, 45.4, 45.1, 46.5
+local timerNextPortal		= mod:NewCDCountTimer(45, 72483, nil, nil, nil, 5, nil, DBM_COMMON_L.HEALER_ICON, true) -- 3s variance [45-48]. Added "keep" arg.
 local timerPortalsOpen		= mod:NewTimer(15, "TimerPortalsOpen", 72483, nil, nil, 6, nil, nil, nil, nil, nil, nil, nil, 72483)
 local timerPortalsClose		= mod:NewTimer(10, "TimerPortalsClose", 72483, nil, nil, 6, nil, nil, nil, nil, nil, nil, nil, 72483)
 local timerHealerBuff		= mod:NewBuffFadesTimer(40, 70873, nil, nil, nil, 5, nil, DBM_COMMON_L.HEALER_ICON)
 local timerGutSpray			= mod:NewBuffFadesTimer(12, 70633, nil, "Tank|Healer", nil, 5)
 local timerCorrosion		= mod:NewBuffFadesTimer(6, 70751, nil, false, nil, 3)
-local timerBlazingSkeleton	= mod:NewNextTimer(50, 70933, "TimerBlazingSkeleton", nil, nil, 1, 17204)
-local timerAbom				= mod:NewNextCountTimer(50, 70922, "TimerAbom", nil, nil, 1)
+local timerBlazingSkeleton	= mod:NewNextTimer(60, 70933, "TimerBlazingSkeleton", nil, nil, 1, 17204)
+local timerAbom				= mod:NewNextCountTimer(60, 70922, "TimerAbom", nil, nil, 1)
 local timerSuppressers		= mod:NewNextCountTimer(60, 70935, nil, nil, nil, 1)
 
 local soundSpecWarnSuppressers	= mod:NewSound(70935)
 
-local berserkTimer			= mod:NewBerserkTimer(420)
+local berserkTimer			= mod:NewBerserkTimer(420) -- Heroic only! Fixed timer: 7min
 
 mod:AddSetIconOption("SetIconOnBlazingSkeleton", 70933, true, 5, {8})
 
@@ -60,6 +60,7 @@ local portalNameH = GetSpellInfo(71987)
 
 local function Suppressers(self)
 	self.vb.SuppressersWave = self.vb.SuppressersWave + 1
+	-- REVIEW! Decay is 1s on every cast
 	if self.vb.SuppressersWave == 2 then
 		timerSuppressers:Stop()
 		timerSuppressers:Start(58, self.vb.SuppressersWave)
@@ -98,13 +99,14 @@ end
 local function StartBlazingSkeletonTimer(self)
 	timerBlazingSkeleton:Start(self.vb.BlazingSkeletonTimer)
 	self:Schedule(self.vb.BlazingSkeletonTimer, StartBlazingSkeletonTimer, self)
-	if self.vb.BlazingSkeletonTimer >= 10 then--Keep it from dropping below 5
-		self.vb.BlazingSkeletonTimer = self.vb.BlazingSkeletonTimer - 5
+	if self.vb.BlazingSkeletonTimer > 5 then --Keep it from dropping below 5, as decay only works if timer is above 5s
+		self.vb.BlazingSkeletonTimer = self.vb.BlazingSkeletonTimer - 5 -- Decay is 5s on every cast
 	end
 end
 
 local function StartAbomTimer(self)
 	self.vb.AbomSpawn = self.vb.AbomSpawn + 1
+	-- REVIEW! Decay is 1s on every cast
 	if self.vb.AbomSpawn == 1 then
 		timerAbom:Start(self.vb.AbomTimer, self.vb.AbomSpawn + 1)--Timer is 60 seconds after first early abom, it's set to 60 on combat start.
 		self:Schedule(self.vb.AbomTimer, StartAbomTimer, self)
@@ -132,32 +134,39 @@ local function Portals(self)
 	timerPortalsClose:Schedule(15)
 	warnPortalSoon:Schedule(40)
 	timerNextPortal:Start(nil, self.vb.portalCount+1)
---	self:Unschedule(Portals)
---	self:Schedule(45.4, Portals, self)--This will never be perfect, since it's never same. 45-48sec variations
 end
 
 -- archmage (all times relative to combat start): 45, 75
 -- zombie: 65,
 function mod:OnCombatStart(delay)
+	-- IEEU: DoAction ACTION_ENTER_COMBAT (boss_valithria_dreamwalker)
 	if self:IsHeroic() then
 		berserkTimer:Start(-delay)
 	end
 	self.vb.portalCount = 0
-	timerNextPortal:Start(nil, 1) -- Hardcode 1 on combatStart, there's no need to calculate self.vb.portalCount+1
+	timerNextPortal:Start(nil, 1) -- 3s variance [45-48]. Hardcode 1 on combatStart, there's no need to calculate self.vb.portalCount+1
 	warnPortalSoon:Schedule(40)
---	self:Schedule(45.4, Portals, self)--This will never be perfect, since it's never same. 45-48sec variations
+	-- SAY_LICH_KING_INTRO: DoAction ACTION_ENTER_COMBAT (npc_the_lich_king_controller)
+	-- Luckily, both IEEU and SAY happen together, so I will keep it all consolidated on IEEU
 	self.vb.BlazingSkeletonTimer = 60
 	self.vb.AbomTimer = 60
 	self.vb.AbomSpawn = 0
-	timerBlazingSkeleton:Start(53-delay)
-	self:Schedule(53-delay, StartBlazingSkeletonTimer, self)
-	timerAbom:Start(22-delay, 1) -- Hardcode 1 on combatStart, there's no need to calculate self.vb.AbomSpawn+1
-	self:Schedule(22-delay, StartAbomTimer, self)
+	-- Gluttonous Abomination
+	timerAbom:Start(5-delay, 1) -- Fixed timer: 5s. Hardcode 1 on combatStart, there's no need to calculate self.vb.AbomSpawn+1
+	self:Schedule(5-delay, StartAbomTimer, self)
+	-- Suppresser
 	self.vb.SuppressersWave = 1
-	timerSuppressers:Start(28-delay, self.vb.SuppressersWave)
-	specWarnSuppressers:Schedule(28)
-	soundSpecWarnSuppressers:Schedule(28, "Interface\\AddOns\\DBM-Core\\sounds\\RaidAbilities\\suppressersSpawned.mp3")
-	self:Schedule(28, Suppressers, self)
+	timerSuppressers:Start(10-delay, self.vb.SuppressersWave) -- Fixed timer: 10s.
+	specWarnSuppressers:Schedule(10)
+	soundSpecWarnSuppressers:Schedule(10, "Interface\\AddOns\\DBM-Core\\sounds\\RaidAbilities\\suppressersSpawned.mp3")
+	self:Schedule(10, Suppressers, self)
+	-- Blistering Zombie
+		-- Fixed timer: 15s
+	-- Risen Archmage
+		-- Fixed timer: 20s
+	-- Blazing Skeleton
+	timerBlazingSkeleton:Start(30-delay) -- Fixed timer: 30s.
+	self:Schedule(30-delay, StartBlazingSkeletonTimer, self)
 end
 
 function mod:SPELL_CAST_START(args)
@@ -225,17 +234,3 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName)
 		Portals(self)
 	end
 end
-
--- I have multiple logs where Yell event is missing due to a bad flag in the SQL, most likely. Best to use boss1 unit events that have proven to be reliable for Warmane, which is also much more efficient
---[[function mod:CHAT_MSG_MONSTER_YELL(msg)
-	if (msg == L.YellPortals or msg:find(L.YellPortals)) and self:LatencyCheck() then
-		self:SendSync("NightmarePortal")
-	end
-end
-
-function mod:OnSync(msg)
-	if msg == "NightmarePortal" and self:IsInCombat() then
-		self:Unschedule(Portals)
-		Portals(self)
-	end
-end]]
